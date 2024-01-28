4c6bd1b7fb05aa9d082a6f3022a90196021c1285
andrey <naumov.a.n@icloud.com>
2024-01-28 21:18:34 +0700

    десериализация

--- src/main/java/ru/gb/Main.java
+++ src/main/java/ru/gb/Main.java
@@ -9,6 +9,13 @@ public class Main {
         System.out.println(person);
 
         Gson gson = new Gson();
-        System.out.println(gson.toJson(person));
+        String textGson = gson.toJson(person);
+        System.out.println(textGson);
+
+        Person deserializedPerson = gson.fromJson(String.valueOf(textGson), Person.class);
+        System.out.printf("first name: %s; last name: %s; age: %d"
+                , deserializedPerson.getFirstName()
+                , deserializedPerson.getLastName()
+                , deserializedPerson.getAge());
     }
 }
