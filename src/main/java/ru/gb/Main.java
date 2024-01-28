657f919d1fe2207cdf033f25235f49ab809105aa
andrey <naumov.a.n@icloud.com>
2024-01-28 21:02:31 +0700

    переопределены методы toString, equals и hashCode

--- src/main/java/ru/gb/Main.java
+++ src/main/java/ru/gb/Main.java
@@ -1,7 +1,11 @@
 package ru.gb;
 
+import com.google.gson.Gson;
+import ru.gb.model.Person;
+
 public class Main {
     public static void main(String[] args) {
-
+        Person person = new Person("Andrey", "Naumov", 41);
+        System.out.println(person);
     }
 }
---
+++ src/main/java/ru/gb/model/Person.java
@@ -0,0 +1,56 @@
+package ru.gb.model;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj);
+    }
+}
